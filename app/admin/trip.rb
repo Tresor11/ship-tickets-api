806a0341ef0f878f9ba7381ef8e6f0b7fd8c2e93
Tresor <tresor.moise2001@gmail.com>
2021-06-25 11:58:10 +0200

    [Migration] update the trip table

--- app/admin/trip.rb
+++ app/admin/trip.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Trip do
-  permit_params :price, :company_id, :ship_id, :departure_city_id, :arrival_city_id, :departure_date, :departure_time, :arrival_time
+  # permit_params :price, :company_id, :ship_id, :departure_city_id, :arrival_city_id, :departure_date, :duration
 end
--- db/migrate/20210625060511_create_trips.rb
+++ db/migrate/20210625060511_create_trips.rb
@@ -6,9 +6,8 @@ class CreateTrips < ActiveRecord::Migration[6.1]
       t.decimal :price
       t.references :ship, null: false, foreign_key: true
       t.references :company, null: false, foreign_key: true
-      t.date :departure_date
-      t.time :departure_time
-      t.time :arrival_time
+      t.datetime :departure_date
+      t.integer :duration
       t.references :departure_city, index: true, foreign_key: { to_table: :cities }
       t.references :arrival_city, index: true, foreign_key: { to_table: :cities }
       t.timestamps
