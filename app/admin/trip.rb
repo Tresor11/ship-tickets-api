4057c4ec521a7d6803a3b306b4786a817fd11bf3
Tresor <tresor.moise2001@gmail.com>
2021-06-26 08:54:17 +0200

    add the associations in the trip model

--- app/admin/trip.rb
+++ app/admin/trip.rb
@@ -2,17 +2,17 @@
 
 ActiveAdmin.register Trip do
   permit_params :price, :company_id, :ship_id,:frequency_id,:departure_city_id, :arrival_city_id, :departure_time, :trip_duration
-  form do |f|
-    f.inputs do
-      f.input :price
-      f.input :company_id
-      f.input :ship_id
-      f.input :frequency_id
-      f.input :departure_city_id
-      f.input :arrival_city_id
-      f.input :departure_time
-      f.input :trip_duration
-    end
-    f.actions
-  end
+  # form do |f|
+  #   f.inputs do
+  #     f.input :price
+  #     f.input :company_id
+  #     f.input :ship_id
+  #     f.input :frequency_id
+  #     f.input :departure_city_id
+  #     f.input :arrival_city_id
+  #     f.input :departure_time
+  #     f.input :trip_duration
+  #   end
+  #   f.actions
+  # end
 end
