5166de77bf22a38978e0e39f07f394c1e9c487b5
Tresor <tresor.moise2001@gmail.com>
2021-06-27 07:44:25 +0200

    update the trip form

--- app/admin/trip.rb
+++ app/admin/trip.rb
@@ -2,17 +2,17 @@
 
 ActiveAdmin.register Trip do
   permit_params :price, :company_id, :ship_id,:frequency_id,:departure_city_id, :arrival_city_id, :departure_time, :trip_duration
-  # form do |f|
-  #   f.inputs do
-  #     f.input :price
-  #     f.input :company_id
-  #     f.input :ship_id
-  #     f.input :frequency_id
-  #     f.input :departure_city_id
-  #     f.input :arrival_city_id
-  #     f.input :departure_time
-  #     f.input :trip_duration
-  #   end
-  #   f.actions
-  # end
+  form do |f|
+    f.inputs do
+      f.input :company_id, :label => 'Company', :as => :select, :collection => Company.pluck(:name,:id)
+      f.input :ship_id, :label => 'Ship', :as => :select, :collection => Ship.pluck(:name,:id)
+      f.input :frequency_id, :label => 'Frequency', :as => :select, :collection => Frequency.pluck(:frequency,:id)
+      f.input :departure_city_id, :label => 'Departure City', :as => :select, :collection => City.pluck(:name,:id)
+      f.input :arrival_city_id, :label => 'Arrival City', :as => :select, :collection => City.pluck(:name,:id)
+      f.input :departure_time
+      f.input :trip_duration
+      f.input :price
+    end
+    f.actions
+  end
 end
