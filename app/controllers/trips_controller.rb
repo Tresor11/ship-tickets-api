97aead9defb527e8c36c5b5f0e921758174d5b90
Tresor <tresor.moise2001@gmail.com>
2021-09-28 14:06:09 +0200

    remove debug statement

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,7 +5,6 @@ class TripsController < ApplicationController
     @trips = Trip.all.includes(:departure_city, :arrival_city, :company, :ship, :frequency, :departure_day)
     query = {}
     query = trips_search_query(params[:search]) if params[:search]
-    binding.pry
     json_response(@trips.where(query))
   end
 
